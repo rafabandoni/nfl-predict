36dbccfeed5c7601a2f15296e533460497ab8448
rafabandoni <rafagbandoni@gmail.com>
2024-07-23 22:38:05 -0300

    fixed run file

--- run.sh
+++ run.sh
@@ -15,10 +15,10 @@ whl_file=$(ls dist/*.whl)
 pip install "$whl_file"
 
 # Rodar a biblioteca instalada
-nfl_predict scrape.yml &&
-nfl_predict modelling_train_data.yml &&
-nfl_predict modelling_averages.yml &&
-nfl_predict training.yml
+nfl_predict confs/scrape.yml &&
+nfl_predict confs/modelling_train_data.yml &&
+nfl_predict confs/modelling_averages.yml &&
+nfl_predict confs/training.yml
 
 # Desativar o ambiente virtual
 deactivate
