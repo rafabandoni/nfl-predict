a250d5583ae1d76d12ac20426c04b1267696d9f1
rafabandoni <rafagbandoni@gmail.com>
2024-07-24 17:14:26 -0300

    changed model to random forest

--- src/nfl_predict/core/training.py
+++ src/nfl_predict/core/training.py
@@ -7,8 +7,12 @@ import typing as T
 import pydantic as pdt
 import pandas as pd
 from abc import ABC, abstractmethod
+from loguru import logger
 
-from sklearn import tree
+
+from sklearn import ensemble
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 # %% READERS
 
@@ -43,6 +47,8 @@ class TrainingData(Training, strict=True, frozen=True, extra="forbid"):
 
     @T.override
     def run(self, data: pd.DataFrame):
+        data['team_a_win'] = [True if x == 'team_a' else False for x in data['outcome']]
+
         features = [
             'team_a_yds',
             'team_a_turnovers',
@@ -53,10 +59,32 @@ class TrainingData(Training, strict=True, frozen=True, extra="forbid"):
         ]
 
         X = data[features]
-        y = data['outcome']
+        y = data['team_a_win']
 
-        arvore = tree.DecisionTreeClassifier()
-        arvore.fit(X,y)
-        return arvore
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+        arvore = ensemble.RandomForestClassifier(random_state=841)
+        param_grid = {
+            'n_estimators': [100, 200,300],
+            'max_depth': [None, 10, 20, 30],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4],
+            'max_features': [None, 'sqrt', 'log2'],
+            'bootstrap': [True, False]
+        }
+        grid_search = GridSearchCV(estimator=arvore, param_grid=param_grid, cv=3, n_jobs=-1, verbose=0)
+        grid_search.fit(X_train,y_train)
+        logger.info("Best parameters found: {}", grid_search.best_params_)
+        best_arvore = grid_search.best_estimator_
+
+        y_pred = best_arvore.predict_proba(X_test)[:, 1]
+
+        valid_score = roc_auc_score(y_test, y_pred)
+        logger.info("Validation ROC-AUC score: {}", valid_score)
+
+        valid_accuracy = accuracy_score(y_test, y_pred > 0.5)
+        logger.info("Accuracy of P>0.5 classifier: {}", valid_accuracy)
+        
+        return best_arvore
 
 TrainingKind = TrainingData
