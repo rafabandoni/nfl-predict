2ec1b9f5085cb1b83cac871369b7d40403bb0d1e
rafabandoni <rafagbandoni@gmail.com>
2025-02-16 15:52:58 -0300

    comecei a refazer o projeto para o curso de predicoes da nfl

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Rodar a biblioteca instalada
-poetry run nfl_predict confs/scrape.yaml &&
-poetry run nfl_predict confs/modelling_train_data.yaml &&
-poetry run nfl_predict confs/modelling_averages.yaml &&
-poetry run nfl_predict confs/training_random_forest.yaml &&
-poetry run nfl_predict confs/training_xgboost.yaml
--- src/nfl_predict/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Predict the NFL Stands."""
